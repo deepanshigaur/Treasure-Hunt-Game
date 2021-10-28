0e46a18e70fddd97b172995b8baf37059a6758dd
Joey Zhang <jzhan372@binghamton.edu>
2021-10-28 02:45:23 -0400

    milestone two

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 def main():
     #Create an instance on your controller object
 	pygame.init()
-	team = {"lead": "Deepanshi Gaur", "backend": "Kweku Antwi-Obeng", "frontend": "?"}
+	team = {"lead": "Deepanshi Gaur", "backend": "Kweku Antwi-Obeng", "frontend": "Joey Zhang"}
 	print("Software Lead is:", team["lead"])
 	print("Backend is:", team["backend"])
 	print("Frontend is:" , team["frontend"])
