54cab7c291465040f1d3e0a1e37a78da324dcdaa
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-10-28 00:54:49 -0400

    milestone 2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #import your controller
+import pygame
 
 def main():
     #Create an instance on your controller object
+	pygame.init()
+	team = {"lead": "Deepanshi Gaur", "backend": "?", "frontend": "?"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:" , team["frontend"])
 main()
