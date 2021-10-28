d433446f273a180eda5d31b063574fea7fa4205d
Kweku Antwi-Obeng <kantwio1@binghamton.edu>
2021-10-28 00:59:54 -0400

    Milestone II(2)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 def main():
     #Create an instance on your controller object
 	pygame.init()
-	team = {"lead": "Deepanshi Gaur", "backend": "?", "frontend": "?"}
+	team = {"lead": "Deepanshi Gaur", "backend": "Kweku Antwi-Obeng", "frontend": "?"}
 	print("Software Lead is:", team["lead"])
 	print("Backend is:", team["backend"])
 	print("Frontend is:" , team["frontend"])
