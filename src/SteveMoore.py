c7987304d03c4ba623a108b101aa8de62a339fa3
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-12-07 22:04:24 -0500

    docstrings

--- src/SteveMoore.py
+++ src/SteveMoore.py
@@ -1,10 +1,14 @@
 import pygame
 import turtle
 import random
-import time
 
 class SteveMoore(pygame.sprite.Sprite):
 	def __init__(self, x, y, image):
+		"""
+	creates the background of the game
+	args: self(class), x, y , image
+	return: None
+		"""
 		pygame.sprite.Sprite._init(self)
 		self.image = pygame.image.load(img_file).convert_alpha()
 		self.rect = self.image.get_rect()
@@ -12,22 +16,47 @@ class SteveMoore(pygame.sprite.Sprite):
 		self.y = y
 		self.rect = rect
 	def forward(self):
+		"""
+	moves the player icon forward
+	args: self(class)
+	return: None
+		"""
 		self.rect.y += y
 	def backward(self):
+		"""
+	moves the player icon backward
+	args: self(class)
+	return: None
+		"""
 		self.rect.y -= y
 	def left(self):
+		"""
+	moves the player icon forward
+	args: self(class)
+	return: None
+		"""
 		self.rect.x -= x
 	def right(self):
+		"""
+	moves the player icon forward
+	args: self(class)
+	return: None
+		"""
 		self.rect.x += x
 
 	
 	def search(self):
+		"""
+	asks user which direction they wanna go to
+	args: self(class)
+	return: None
+		"""
 		self.state == "GAME"
 		ques1 = input("Would you like to go forward(F) or backward(B) ?")
-	        ques2 = input("Would you like to go right(R) or left(L) ?")
+		ques2 = input("Would you like to go right(R) or left(L) ?")
 
 		if ques1 == 'F':
-			 = self.rect.y -= self.speed
+			= self.rect.y -= self.speed
 		elif ques1 == 'B':
 			= self.rect.y += self.speed
 		else:
@@ -39,6 +68,11 @@ class SteveMoore(pygame.sprite.Sprite):
 		else:
 			none ### add kill switch
 	def guess(self, room):
+		"""
+	prints you guessed right if the player succesafully finds the treasure
+	args: self(class)
+	return: None
+		"""
 		ingusnum == 0
 		if right == True
 			print("You guessed right!!!!!!!")
