2806e08b488233bfbfc141e7dae0f50230bcfa33
deepanshigaur <89813272+deepanshigaur@users.noreply.github.com>
2021-12-09 02:52:02 -0500

    ---

--- src/controller.py
+++ src/controller.py
@@ -28,42 +28,42 @@ class controller():
             self.update()
             self.render()
             
-        def get_events(self):
+    def get_events(self):
             '''
         sets up if the user presses a certain button, if its true runs the code and if its false dont do anything
         args: self(class)
         return: None
             '''
             #while self.state == "GAME":
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
                     self.playing = False
                     self.running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.playing = False
-                        self.running = False
-                    if event.key == pygame.K_l:
-                        self.actions['left'] = True
-                    if event.key == pygame.K_r:
-                        self.actions['right'] = True
-                    if event.key == pygame.K_d:
-                        self.actions['down'] = True
-                    if event.key == pygame.K_u:
-                        self.actions['up'] = True
-                    if event.key == pygame.K_RETURN:
-                        self.actions['start'] = True
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_l:
-                        self.actions['left'] = False
-                    if event.key == pygame.K_r:
-                        self.actions['right'] = False
-                    if event.key == pygame.K_d:
-                        self.actions['down'] = False
-                    if event.key == pygame.K_u:
-                        self.actions['up'] = False
-                    if event.key == pygame.K_RETURN:
-                        self.actions['start'] = False
+                if event.key == pygame.K_l:
+                    self.actions['left'] = True
+                if event.key == pygame.K_r:
+                    self.actions['right'] = True
+                if event.key == pygame.K_d:
+                    self.actions['down'] = True
+                if event.key == pygame.K_u:
+                    self.actions['up'] = True
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_l:
+                    self.actions['left'] = False
+                if event.key == pygame.K_r:
+                    self.actions['right'] = False
+                if event.key == pygame.K_d:
+                    self.actions['down'] = False
+                if event.key == pygame.K_u:
+                    self.actions['up'] = False
+                if event.key == pygame.K_RETURN:
+                    self.actions['start'] = False
 
        def update(self):
             '''
@@ -79,23 +79,23 @@ class controller():
         args: self(class), display(class)
         return: None
             '''
-            self.state_stack[-1].render(self.game_canvas)
-            self.screens.blit(pygame.transform.scale(self.game_canvas,(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0,0))
-            display.blit(self.room_img, (0,0))
-            self.user.render(display)
+           self.state_stack[-1].render(self.game_canvas)
+           self.screens.blit(pygame.transform.scale(self.game_canvas,(self.SCREEN_WIDTH, self.SCREEN_HEIGHT)), (0,0))
+           display.blit(self.room_img, (0,0))
+           self.user.render(display)
 
-            display.fill((255,255,255))
-            self.controller.draw_text(display, "Game", (0,0,0), self.controller.width/2, self.controller.height/2)
+           display.fill((255,255,255))
+           self.controller.draw_text(display, "Game", (0,0,0), self.controller.width/2, self.controller.height/2)
 
-            display.fill((255, 255, 255))
-            self.controller.draw_text(display, "Steven Moore's Treasure Adventure!", (0, 0, 0),
+           display.fill((255, 255, 255))
+           self.controller.draw_text(display, "Steven Moore's Treasure Adventure!", (0, 0, 0),
 self.controller.width/2, self.controller.height/6)
 
-            self.prev_state.render(display)
-            display.blit(self.menu_img, self.menu_rect)
-            display.blit(self.cursor_img, self.cursor_rect)
-            display.blit(self.curr_image, (self.position_x, position_y))
-            pygame.display.flip()
+           self.prev_state.render(display)
+           display.blit(self.menu_img, self.menu_rect)
+           display.blit(self.cursor_img, self.cursor_rect)
+           display.blit(self.curr_image, (self.position_x, position_y))
+           pygame.display.flip()
          
   
        def draw_text(self, surface, text, color, x, y):
@@ -104,11 +104,11 @@ self.controller.width/2, self.controller.height/6)
         args: self(class), surface(str), text(str), color(str), x(int), y(int)
         return: None
             '''
-            text_surface = self.font.render(text, True, color)
+           text_surface = self.font.render(text, True, color)
             #text_surface.set_colorkey((0,0,0))
-            text_rect = text_surface.get_rect()
-            text_rect.center = (x, y)
-            surface.blit(text_surface, text_rect)
+           text_rect = text_surface.get_rect()
+           text_rect.center = (x, y)
+           surface.blit(text_surface, text_rect)
 
        def load_assets(self):
             '''
@@ -116,10 +116,10 @@ self.controller.width/2, self.controller.height/6)
         args: self(class), surface(str), text(str), color(str), x(int), y(int)
         return: None
             '''
-            self.assets_dir = os.path.join("assets")
-            self.sprite_dir = os.path.join(self.assets_dir, "sprites")
-            self.font_dir = os.path.join(self.assets_dir, "font")
-            self.font = pygame.font.Font(os.path.join(self.font_dir, "Pokemon GB.ttf"), 15)
+           self.assets_dir = os.path.join("assets")
+           self.sprite_dir = os.path.join(self.assets_dir, "sprites")
+           self.font_dir = os.path.join(self.assets_dir, "font")
+           self.font = pygame.font.Font(os.path.join(self.font_dir, "Pokemon GB.ttf"), 15)
 
        def load_states(self):
             '''
@@ -127,8 +127,8 @@ self.controller.width/2, self.controller.height/6)
         args: self(class), surface(str), text(str), color(str), x(int), y(int)
         return: None
             '''
-            self.screen_screen = screen(self)
-            self.state_stack.append(self.screen_screen)
+           self.screen_screen = screen(self)
+           self.state_stack.append(self.screen_screen)
 
        def reset_keys(self):
             '''
@@ -136,19 +136,20 @@ self.controller.width/2, self.controller.height/6)
         args: self(class)
         return: None
             '''
-            for action in self.actions:
-                self.actions[action] = False
+           for action in self.actions:
+               self.actions[action] = False
                 
-            if __name__ == "__main__":
-            stuff = controller()
-            while stuff.gaming:
-            stuff.game_loop()
+           if __name__ == "__main__":
+           stuff = controller()
+           while stuff.gaming:
+           stuff.game_loop()
 
         def win(self):
             font = pygame.font.Font('freesansbold.ttf', 32)
             text = font.render("Congrats, you found the treasure!", True, (0,255,0))
             self.canvas.blit(text, (650, 40))
             pygame.display.update
+            
         def gameOver(self):
             self.hero.kill()
             myfont = pygame.font.SysFont(None, 30)
